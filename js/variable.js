99150e7bd9eb88f4165a5022afa7fc88cfa1ac93
Hugo Chavez <Hugo@LAPTOP-LENOY700>
2021-10-18 14:29:34 -0300

    clase del 18 de octubre

---
+++ js/variable.js
@@ -0,0 +1,14 @@
+var nombre = 'Fulano';
+var edad = 10;
+var altura = 1.54;
+var soltero = true;
+
+document.write('NOMBRE : ' + nombre);
+
+document.write('<br>');
+document.write(edad);
+document.write('<br>');
+document.write(altura);
+document.write('<br>');
+document.write(soltero);
+
